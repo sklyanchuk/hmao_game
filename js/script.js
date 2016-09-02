8aa18f949604aa17333caf33a7e47ec67b0ee475
sklyanchuk <artem.sklyanchuk@gmail.com>
2016-09-02 15:51:41 +0300

    Changed dialog style (no alerts) and other small changes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,48 @@
-alert('Приветствую тебя, искатель приключений! Сегодня ты окунешься в потрясающий мир проекта ХМАО! Для начала давай познакомимся с главным персонажем нашей игры, кликни в фигурку человечка в центре страницы.');
+document.getElementById('tolya').addEventListener ('click', playGame);
 
-document.getElementById('tolya').onclick = function() {
+function playGame() {
+  document.getElementById('tolya').removeEventListener('click', playGame);
+
   this.style.left = this.getBoundingClientRect().left + 'px';
   this.style.top = this.getBoundingClientRect().top + 'px';
-
   this.style.position = 'fixed';
-  alert("Привет! Я Толик! Теперь ты можешь мной управлять! (короч, юзай кнопки влево, вверх там и т.д.)");
-};
+
+  var commentContainer = document.getElementById('comment');
+  var commentIntro = '- Привет! Я Толик! Теперь ты можешь мной управлять с помощью стрелочек на твоей клавиатуре. (Которая, конечно же, сделана под брендом DEPO?) Посмотри, похоже, тебе кто-то звонит!';
+  commentContainer.innerHTML = commentIntro;
+  phone.classList.remove('mini-phone-disabled');
+  phone.classList.add('mini-phone-enabled');
+
+  document.getElementById('phone').addEventListener ('click', callPhone);
+
+  function callPhone() {
+    document.getElementById('phone').removeEventListener ('click', callPhone);
+
+    var commentDmitrishinCallFirst = 'Вы поднимаете трубку и слышите голос: "Толя, это Дмитришин, я тебе прислал письмо, мне срочно нужен ответ, посмотри. Ты понял?". Голос Дмитришина показался Вам очень грубым. Хотите ли послать его куда подальше?';
+    commentContainer.innerHTML = commentDmitrishinCallFirst;
+    this.classList.remove('mini-phone-enabled');
+    this.classList.add('mini-phone-disabled');
+
+    showButtons();
+
+    noButton.addEventListener('click', noButtonClick);
+    yesButton.addEventListener('click', yesButtonClick);
+
+    function noButtonClick() {
+      hideButtons();
+      var commentMailFirst = 'У вас невероятное самообладание! Идите уже прочтите это злосчастное письмо!';
+      commentContainer.innerHTML = commentMailFirst;
+      mail.classList.add('animate');
+    }
+
+    function yesButtonClick() {
+      hideButtons();
+      var commentDmitrishinCallCherkasov = 'Дмитришин позвонил Черкасову и теперь Вас вызываеют к нему в кабинет. Похоже, у вас неприятности...';
+      commentContainer.innerHTML = commentDmitrishinCallCherkasov;
+      kabinet.classList.add('animate');
+    }
+  }
+}
 
 document.getElementById('tolya').onkeydown = function(e) {
   switch (e.keyCode) {
@@ -25,18 +61,14 @@ document.getElementById('tolya').onkeydown = function(e) {
     }
 };
 
-document.getElementById('phone').onclick = function() {
-  alert('Вы поднимаете трубку и слышите голос: "Толя, это Дмитришин, я тебе прислал письмо, мне срочно нужен ответ, посмотри. Ты понял?"');
-  var desition = confirm('Голос Дмитришина показался Вам очень грубым. Хотите ли послать его куда подальше?');
-  if (desition) {
-    alert('Дмитришин позвонил Черкасову и теперь Вас вызываеют к нему в кабинет. Похоже, у вас неприятности...');
-    kabinet.classList.add('animate');
-  } else {
-    alert('Невероятное самообладание! Идите уже прочтите это злосчастное письмо!');
-    kabinet.classList.add('animate');
-  }
-  this.classList.remove('mini-phone-enabled');
-  this.classList.add('mini-phone-disabled');
-};
+function showButtons () {
+  document.getElementById('noButton').style.display='inline';
+  document.getElementById('yesButton').style.display='inline';
+}
+
+function hideButtons () {
+  document.getElementById('noButton').style.display='none';
+  document.getElementById('yesButton').style.display='none';
+}
 
 // Плохое решение. Вы отстранены от проекта ХМАО. Конец игры!
