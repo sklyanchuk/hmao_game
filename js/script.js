a705e183b92e66f090fa7b972dcec13a33ca5a3d
sklyanchuk <artem.sklyanchuk@gmail.com>
2016-09-02 19:07:02 +0300

    More funny stuff :)

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function playGame() {
   function callPhone() {
     document.getElementById('phone').removeEventListener ('click', callPhone);
 
-    var commentDmitrishinCallFirst = 'Вы поднимаете трубку и слышите голос: "Толя, это Дмитришин, я тебе прислал письмо, мне срочно нужен ответ, посмотри. Ты понял?". Голос Дмитришина показался Вам очень грубым. Хотите ли послать его куда подальше?';
+    var commentDmitrishinCallFirst = 'Ты поднимаешь трубку и слышишь голос: "Толя, это Дмитришин, я тебе прислал письмо, мне срочно нужен ответ, посмотри. Ты понял?". Голос Дмитришина показался тебе очень грубым. Хочешь ли послать его куда подальше?';
     commentContainer.innerHTML = commentDmitrishinCallFirst;
     this.classList.remove('mini-phone-enabled');
     this.classList.add('mini-phone-disabled');
@@ -30,14 +30,14 @@ function playGame() {
 
     function noButtonClick() {
       hideButtons();
-      var commentMailFirst = 'У вас невероятное самообладание! Идите уже прочтите это злосчастное письмо!';
+      var commentMailFirst = 'У тебя нереальное терпение! Вот это самоконтроль! Иди прочти уже это злосчастное письмо.';
       commentContainer.innerHTML = commentMailFirst;
       mail.classList.add('animate');
     }
 
     function yesButtonClick() {
       hideButtons();
-      var commentDmitrishinCallCherkasov = 'Дмитришин позвонил Черкасову и теперь Вас вызываеют к нему в кабинет. Похоже, у вас неприятности...';
+      var commentDmitrishinCallCherkasov = 'Дмитришин позвонил Черкасову и теперь тебя вызывают к нему в кабинет. Похоже, у тебя неприятности...';
       commentContainer.innerHTML = commentDmitrishinCallCherkasov;
       kabinet.classList.add('animate');
     }
@@ -61,6 +61,39 @@ document.getElementById('tolya').onkeydown = function(e) {
     }
 };
 
+// Подсказки
+document.getElementById('andrew1').addEventListener ('click', getHelp1);
+function getHelp1() {
+  andrew1.classList.add('andrew_used');
+  var hint1 = '&#171Толя, блядь! Покровительство - это, конечно, хорошо, но до опредленных пределов!&#187';
+  hint_text.innerHTML = hint1;
+  document.getElementById('hint').style.display = 'block';
+  document.getElementById('andrew1').removeEventListener ('click', getHelp);
+}
+
+document.getElementById('andrew2').addEventListener ('click', getHelp2);
+function getHelp2() {
+  andrew2.classList.add('andrew_used');
+  var hint2 = '&#171Учись задавать вопросы, блядь, Толя!&#187';
+  hint_text.innerHTML = hint2;
+  document.getElementById('hint').style.display='block';
+  document.getElementById('andrew2').removeEventListener ('click', getHelp);
+}
+document.getElementById('andrew3').addEventListener ('click', getHelp3);
+function getHelp3() {
+  andrew3.classList.add('andrew_used');
+  var hint3 = '&#171Контролируй доставку корреспонденции, Толя!&#187';
+  hint_text.innerHTML = hint3;
+  document.getElementById('hint').style.display='block';
+  document.getElementById('andrew3').removeEventListener ('click', getHelp);
+}
+//
+
+document.getElementById('close').addEventListener ('click', closeHint);
+function closeHint() {
+  document.getElementById('hint').style.display = 'none';
+}
+
 function showButtons () {
   document.getElementById('noButton').style.display='inline';
   document.getElementById('yesButton').style.display='inline';
@@ -70,5 +103,4 @@ function hideButtons () {
   document.getElementById('noButton').style.display='none';
   document.getElementById('yesButton').style.display='none';
 }
-
 // Плохое решение. Вы отстранены от проекта ХМАО. Конец игры!
