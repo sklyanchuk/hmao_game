9262681e3b295849aad456173636bbbbf9ebfb0e
sklyanchuk <artem.sklyanchuk@gmail.com>
2016-09-03 12:26:00 +0300

    Added new dialogs

--- js/script.js
+++ js/script.js
@@ -21,7 +21,6 @@ function firstMove() {
   phone.classList.add('mini-phone-enabled');
 
   document.getElementById('phone').addEventListener ('click', callPhone);
-
   function callPhone() {
     document.getElementById('phone').removeEventListener ('click', callPhone);
 
@@ -40,6 +39,44 @@ function firstMove() {
       var commentMailFirst = 'У тебя нереальное терпение! Вот это самоконтроль! Иди прочти уже это злосчастное письмо.';
       commentContainer.innerHTML = commentMailFirst;
       mail.classList.add('animate');
+      document.getElementById('mail').addEventListener ('click', readEmail);
+      function readEmail() {
+        var email1 = 'Ты открыл Оутлук, и среди кучи непрочитанных писем нашел письмо от Димтришина: "Толя, где расчеты, блять? Я какой день жду. Если сегодня их у меня не будет, я ищу другого поставщика!!!" Ты пиздец растерян. Что будешь делать?';
+        commentContainer.innerHTML = email1;
+        mail.classList.remove('animate');
+
+        document.getElementById('options').style.display='block';
+        var optionFirst = 'Нужно подойти к Лене и обсудить расчет';
+        option1.innerHTML = optionFirst;
+        var optionSecond = 'Сделать расчет самому';
+        option2.innerHTML = optionSecond;
+        var optionThird = 'Посовещаться с Селезневым';
+        option3.innerHTML = optionThird;
+
+        document.getElementById('option1').addEventListener ('click', doOption1);
+        function doOption1() {
+          var lenaWait = 'Лена ждет тебя у своего рабочего места!';
+          commentContainer.innerHTML = lenaWait;
+          document.getElementById('options').style.display='none';
+          lena.classList.add('animate');
+        }
+        document.getElementById('option2').addEventListener ('click', doOption2);
+        function doOption2() {
+          var doMath = 'Бросай свою жопу на стул и давай заполняй PL';
+          commentContainer.innerHTML = doMath;
+          document.getElementById('options').style.display='none';
+          pl.classList.add('animate');
+        }
+
+        document.getElementById('option3').addEventListener ('click', doOption3);
+        function doOption3() {
+          var findSeleznev = 'Нужно поймать Селезнева, он все летает где-то тут по офису.';
+          commentContainer.innerHTML = findSeleznev;
+          document.getElementById('options').style.display='none';
+          seleznev.classList.add('animate');
+        }
+        document.getElementById('mail').removeEventListener ('click', readEmail);
+      }
     }
 
     function yesButtonClick() {
@@ -47,6 +84,85 @@ function firstMove() {
       var commentDmitrishinCallCherkasov = 'Дмитришин позвонил Черкасову и теперь тебя вызывают к нему в кабинет. Похоже, у тебя неприятности...';
       commentContainer.innerHTML = commentDmitrishinCallCherkasov;
       kabinet.classList.add('animate');
+      document.getElementById('kabinet').addEventListener ('click', visitCherkasov);
+      function visitCherkasov() {
+        var visit1 = 'Ты входишь в кабинет Черкасова. Он стоит и зло смотрит на тебя! "Толя, меня твой Дмитришин заебал. Избавь меня, пожалуйста, от него!!" Как поступишь?';
+        commentContainer.innerHTML = visit1;
+        kabinet.classList.remove('animate');
+        document.getElementById('options').style.display='block';
+
+        var optionFirst = 'Найти виновных!!!';
+        option1.innerHTML = optionFirst;
+        var optionSecond = 'Попросить Лену отстранить тебя от проекта!';
+        option2.innerHTML = optionSecond;
+        var optionThird = 'Извиниться и пообещать все исправить';
+        option3.innerHTML = optionThird;
+
+        document.getElementById('option1').addEventListener ('click', doOption1);
+        function doOption1() {
+          var commentFindGuilty = 'Будем искать виновных! Кликни на виноватого!';
+          commentContainer.innerHTML = commentFindGuilty;
+          document.getElementById('options').style.display='none';
+          kabinet.classList.add('animate');
+          lena.classList.add('animate');
+          seleznev.classList.add('animate');
+          document.getElementById('option1').removeEventListener ('click', doOption1);
+        }
+        document.getElementById('option2').addEventListener ('click', doOption2);
+        function doOption2() {
+          var lenaWait = 'Лена ждет тебя у своего рабочего места!';
+          commentContainer.innerHTML = lenaWait;
+          document.getElementById('options').style.display='none';
+          lena.classList.add('animate');
+          document.getElementById('option2').removeEventListener ('click', doOption2);
+        }
+
+        document.getElementById('option3').addEventListener ('click', doOption3);
+        function doOption3() {
+          var appologize = 'Вы дико извиняетесь, говорите много красивых слов, обещаете непременно все-все исправить и, вроде, Черкасов немного успокаивается. Но вам все равно нужно разобраться с этим злосчастным письмом...';
+          commentContainer.innerHTML = appologize;
+          document.getElementById('options').style.display='none';
+          mail.classList.add('animate');
+          document.getElementById('mail').addEventListener ('click', readEmail);
+          function readEmail() {
+            var email1 = 'Ты открыл Оутлук, и среди кучи непрочитанных писем нашел письмо от Димтришина: "Толя, где расчеты, блять? Я какой день жду. Если сегодня их у меня не будет, я ищу другого поставщика!!!" Ты пиздец растерян. Что будешь делать?';
+            commentContainer.innerHTML = email1;
+            mail.classList.remove('animate');
+
+            document.getElementById('options').style.display='block';
+            var optionFirst = 'Нужно подойти к Лене и обсудить расчет';
+            option1.innerHTML = optionFirst;
+            var optionSecond = 'Сделать расчет самому';
+            option2.innerHTML = optionSecond;
+            var optionThird = 'Посовещаться с Селезневым';
+            option3.innerHTML = optionThird;
+
+            document.getElementById('option1').addEventListener ('click', doOption1);
+            function doOption1() {
+              var lenaWait = 'Лена ждет тебя у своего рабочего места!';
+              commentContainer.innerHTML = lenaWait;
+              document.getElementById('options').style.display='none';
+              lena.classList.add('animate');
+            }
+            document.getElementById('option2').addEventListener ('click', doOption2);
+            function doOption2() {
+              var doMath = 'Сади свою жопу на стул и давай заполняй PL';
+              commentContainer.innerHTML = doMath;
+              document.getElementById('options').style.display='none';
+              pl.classList.add('animate');
+            }
+
+            document.getElementById('option3').addEventListener ('click', doOption3);
+            function doOption3() {
+              var findSeleznev = 'Нужно поймать Селезнева, он все летает где-то тут по офису.';
+              commentContainer.innerHTML = findSeleznev;
+              document.getElementById('options').style.display='none';
+              seleznev.classList.add('animate');
+            }
+          }
+        }
+        document.getElementById('kabinet').removeEventListener ('click', visitCherkasov);
+      }
     }
   }
 }
